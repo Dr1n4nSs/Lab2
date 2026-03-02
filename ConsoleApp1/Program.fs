cec173fb0e674954d85fabda2d15f5548ff622b5
Dr1v4nSs <104269035+Dr1n4nSs@users.noreply.github.com>
2026-03-02 06:24:01 +0500

    Add files via upload

---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+[<EntryPoint>]
+printf "Создание списка модулей введённых элементов\n"
+printf "Введите количество чисел - "
+let countInput  = Console.ReadLine()
+
+match Int32.TryParse(countInput) with
+| true, n when n > 0 ->
+    let mutable spisok = []
+    for i in 1..n do
+        printf"\nВведите элемент %d - " i
+        let mutable number = Console.ReadLine()        
+        match Double.TryParse(number) with
+        | true, value ->
+            spisok <- abs(value) :: spisok 
+        | _ ->
+            printf "\nВведено некорректное значение элемента"
+    let spisok = List.rev spisok
+    printf"\nРезультат - %A" spisok
+| true, 0 ->
+    printf "Колличество элементов равно 0, список будет пустым"
+| _ ->
+    printf "Введено некорректное число элементов"
---
+++ ConsoleApp2/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+[<EntryPoint>]
+printf "Вычисление факториала числа\n"
+printf "Введите число - "
+let countInput  = Console.ReadLine()
+
+let rec factorial n=
+    if n <= 1 then 
+        1
+    else 
+        n * factorial (n - 1)
+
+match Int32.TryParse(countInput) with
+| true, n when n >= 0 ->
+    let result = factorial n
+    printfn "Факториал %d равен %d" n result
+| true, _ ->
+    printf "Введено отрицательное число, для него не определён факториал"
+| _ ->
+    printf "Введено некорректное число для вычисления факториала"
